b03f21441794012da0dcfbed52cf645b57cb861c
LivioLima <livio.lima.br@gmail.com>
2016-12-02 12:39:43 -0300

    added values for ambient, diffuse and expecular lighting on scene class part 2.

--- scene.cpp
+++ scene.cpp
@@ -4,10 +4,26 @@
 
 Scene::Scene()
 {
-    //This must contain ours objects and the lighting.
+    //This must contain ours objects and the lighting.,
 
-    ilumination_xyz_position[0] = 0;
-    ilumination_xyz_position[1] = 0;
-    ilumination_xyz_position[2] = 0;
+    ilumination_xyz_position = NumberVector(20, 30, 40);
+
+      ambient[0] = 0.0f;
+      ambient[1] = 0.0f;
+      ambient[2] = 0.0f;
+      ambient[3] = 1.0f;
+
+      diffuse[0] = 1.0f;
+      diffuse[1] = 1.0f;
+      diffuse[2] = 1.0f;
+      diffuse[3] = 1.0f;
+
+      specular[0] = 1.0f;
+      specular[1] = 1.0f;
+      specular[2] = 1.0f;
+      specular[3] = 1.0f;
+
+      //It is necessary calculate vector n,l,v,r
+
 
 }
