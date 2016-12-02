479542c17c19f82c92d02b7b892b71a53e4072ca
LivioLima <livio.lima.br@gmail.com>
2016-12-02 14:48:52 -0300

    valeus for ambient, diffuse and specular lighting fixed.

--- scene.h
+++ scene.h
@@ -9,7 +9,9 @@ public:
     Scene();
 
     Triangle triangle;
-    int ilumination_xyz_position[3];
+    NumberVector ilumination_xyz_position;
+
+    double ambient[4], diffuse[4], specular[4];
 };
 
 #endif // SCENE_H
